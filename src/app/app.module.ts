436eb7f95884fdf4e3a68201a984e5d2b87973dd
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-29 09:41:14 +0530

    added usdata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewPassengerDataComponent } from './view-passenger-data/view-passenger-data.component';
+import { UsPublicDataComponent } from './us-public-data/us-public-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewPassengerDataComponent
+    ViewPassengerDataComponent,
+    UsPublicDataComponent
   ],
   imports: [
     BrowserModule,
