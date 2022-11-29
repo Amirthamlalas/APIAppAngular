6c52f9c14ec1db6f473e0b7b9a19eddcfe796915
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-29 10:06:06 +0530

    added userinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ViewPassengerDataComponent } from './view-passenger-data/view-passenger-data.component';
 import { UsPublicDataComponent } from './us-public-data/us-public-data.component';
 import { PublicAPILinksComponent } from './public-apilinks/public-apilinks.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewPassengerDataComponent,
     UsPublicDataComponent,
-    PublicAPILinksComponent
+    PublicAPILinksComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
